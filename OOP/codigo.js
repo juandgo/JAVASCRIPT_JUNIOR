3564f1784f4c432b322a86e672fa5b4a5eb614ca
JuanDa97 <juandavidgo1997@gmail.com>
2021-08-25 15:38:34 -0500

    fin de ejemplo de OOP

--- OOP/codigo.js
+++ OOP/codigo.js
@@ -1,29 +1,43 @@
-class animal{
+class Animal{
     constructor(especie, edad, color){
         this.especie = especie;
         this.edad = edad;
         this.color = color;
-        this.info = `Soy ${this.especie}, tengo ${this.edad} y soy de colo ${this.color}`;
+        this.info = `Soy ${this.especie}, tengo ${this.edad} y soy de color ${this.color}`;
     }
     //La funcion felcha no puede ser usada dentro de una clase
     verInfo(){
-        document.write(this.info + "<br>")
+        document.write(this.info + "<br>");
     };
 }
 
-class perro extends animal{
+class Perro extends Animal{
     constructor(especie, edad, color, raza){
-        super(especie,edad);
+        super(especie,edad, color);
+        this.raza = raza;
+    }
+
+    // static ladrar(){
+    //     alert("¡Wow!");
+    // }
+
+    set setRaza(newName){
+        this.raza = newName;
+    }
+
+    get getRaza(){
+        return this.raza;
     }
 }
 
-let perro = new animal("perro",5,"rojo");
-let gato = new animal("gato",3,"negro");
-let pajaro = new animal("pajaro",2,"verde");
+const perro = new Perro("perro",5,"rojo","Doberman");
+const gato = new Animal("gato",3,"negro");
+const pajaro = new Animal("pajaro",2,"verde");
 //  console.log(perro);
 //  document.write(perro.color);
 
-perro.verInfo();
-gato.verInfo();
-pajaro.verInfo();
+perro.setRaza = "Pedro";
+document.write(perro.getRaza)
+// Perro.ladrar();
+// perro.verInfo();
 
