dce6d09c3f9aa0a7345bc39c218d41feda02b862
JuanDa97 <juandavidgo1997@gmail.com>
2022-02-08 17:07:36 -0500

    contento

--- OOP/problema 3/codigo.js
+++ OOP/problema 3/codigo.js
@@ -3,5 +3,42 @@ class App{
         this.descargas = descargas;
         this.puntuacion = puntuacion;
         this.peso = peso;
+        this.iniciada = false;
+        this.instalada = false;
     }
+
+    abrir(){
+        if(this.iniciada == false && this.instalada == true){
+            this.iniciada = true;
+            alert("app iniciada");
+        }
+    }
+
+    cerrar(){
+        if(this.iniciada == true && this.instalada == true){
+            this.iniciada = false;
+            alert("app cerrada");
+        }
+    }
+
+    instalar(){
+        if(this.instalada == false){
+            this.instalada = true;
+            alert("app instalada correctamente");
+        }
+    }
+
+    desinstalar(){
+        if(this.instalada == true){
+            this.instalada = false;
+            alert("app desinstalada correctamente");
+        }
+    }
+
+    app = new App("16.000","5 estrellas","900mb");
+
+    app.instalar();
+    app.cerrar();
+    app.iniciar();
+
 }
