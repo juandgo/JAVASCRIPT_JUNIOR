3092616a12577321da2eb641cdcafe0c23da3930
JuanDa <juandavidgo1997@gmail.com>
2022-02-10 16:24:20 -0500

    fghj

--- OOP/problema 3/codigo.js
+++ OOP/problema 3/codigo.js
@@ -34,11 +34,34 @@ class App{
             alert("app cerrada");  
         }
     }
+
+    appInfo(){
+        return `
+        Descargas: <b>${this.descargas}</b></br>
+        Puntuacion: <b>${this.puntuacion}</b></br>
+        Peso: <b>${this.peso}</b></br>
+        `
+    }
 }
 
 app = new App("16.000","5 estrellas","900mb");
+app2 = new App("1.000","4 estrellas","400mb");
+app3 = new App("6.000","5 estrellas","100mb");
+app4 = new App("23.000","4 estrellas","1gb");
+app5 = new App("900","3 estrellas","250mb");
+app6 = new App("17","2 estrellas","900mb");
 
-app.instalar()
-app.abrir();
-app.cerrar();
-app.desinstalar();
+document.write(`
+    ${app.appInfo()}</b></br>
+    ${app2.appInfo()}</b></br>
+    ${app3.appInfo()}</b></br>
+    ${app4.appInfo()}</b></br>
+    ${app5.appInfo()}</b></br>
+    ${app6.appInfo()}</b></br>
+    `)
+
+
+// app.instalar()
+// app.abrir();
+// app.cerrar();
+// app.desinstalar();
