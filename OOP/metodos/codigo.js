c0b258d1ef4c56143485502128a3a7b5a082aedd
JuanDa97 <juandavidgo1997@gmail.com>
2022-02-14 15:48:14 -0500

    Metodos

---
+++ OOP/metodos/codigo.js
@@ -0,0 +1,48 @@
+// ========== CONCAT ======================
+
+// let cadena = "cadena de prueba";
+// let cadena2 = " real";
+
+// let resultado = cadena.concat("  hola mundo",cadena2);
+
+// document.write(resultado);
+
+// ========== STARWITH, ENDSWITH, INCLUDES ======================
+//STARWITH indica si es cierto que comienza con la misma plabra 
+//ENDSWITH indica si es cierto que termina con la misma plabra 
+//INCLUDES indica si es cierto que la palabra se encuntra en el texto 
+
+// let cadena = "cadena de prueba";
+// let cadena2 = "cadena";
+// let cadena3 = "de";
+
+// let resultado = cadena.startsWith(cadena2);
+// let resultado2 = cadena.endsWith(cadena3);
+// let resultado3 = cadena.includes(cadena3);
+
+// document.write(resultado);
+// document.write("<br>",resultado2);
+// document.write("<br>",resultado3);
+
+//============ INDEXOFF, LASTINDEXOFF  ===========================
+// Me da la posicion de la cadena en que inicia la palabra 
+//lastIndexOf busca la palabra en la ultima posicion de la cadena
+
+// let cadena = "cadena de prueba";
+// let cadena2 = "prueba";
+
+// let resultado = cadena.indexOf(cadena2);
+// let resultado2 = cadena.lastIndexOf(cadena2);
+
+// document.write(resultado);
+// document.write(resultado2);
+
+//============  PADSTART  =======================================
+//Agrega texto segun la cantidad de caracteres indicada.
+
+// let cadena = "ABC";
+// let cadena2 = "";
+
+// let resultado = cadena.padStart(40,"1234");
+
+// document.write(resultado);
