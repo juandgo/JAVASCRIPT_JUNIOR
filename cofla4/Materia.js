3528e6b2eff5470808a4161429c2a8bfff7ce8ec
JuanDa97 <juandavidgo1997@gmail.com>
2022-03-09 14:16:38 -0500

    cofla 4

--- cofla4/Materia.js
+++ cofla4/Materia.js
@@ -2,9 +2,9 @@
 const obtenerInformacion = (materia) => {
     materias = {
         fisica: ["Roldan","pedro","camilo","ramiro","armando"],
-        progrmacion: ["Dalto","maria","juancho","diana","armando"],
-        logica: ["Claudia","felipe","natahlia","fernando","Cofla","julian"],
-        quimica: ["Andres","julian","camilo","diana","maria","Cofla"],
+        programacion: ["Dalto","maria","juancho","diana","armando"],
+        logica: ["Claudia","felipe","natahlia","fernando","cofla","julian"],
+        quimica: ["Andres","julian","camilo","diana","maria","cofla"],
     }
     if (materias[materia] !== undefined) {
         // return materias[materia]
@@ -14,27 +14,26 @@ const obtenerInformacion = (materia) => {
     }
 }
 const mostrarInformacion = (materia)=>{
-    let informacion = informacion(materia);
+    let informacion = obtenerInformacion(materia);
     if (informacion !== false) {
-        let profesor = informacion(materia)[0][0]
-        let alumnos = informacion(materia)[0]
+        let profesor = informacion[0][0];
+        alumnos = informacion[0];
         alumnos.shift();//quita el primer elemento del arreglo
         document.write(`El profesor de <b>${informacion[1]}</b>: <b style="color: red">${profesor}</b><br>
                     Los alumnos son: <b style="color: blue">${alumnos}</b><br><br>`)
     }
 }
 
-const cantidadClases = (alumnos) => {
-    let informacion = informacion();
+const cantidadClases = (alumno) => {
+    let informacion = obtenerInformacion();
     let cantidadTotal = 0;
     for(info in informacion){
-        if (informacion[2][info]) {
-            if (informacion[2][info].includes(alumno)) {
-                cantidadTotal++
-            }
+        // document.write(info)
+        if (informacion[info].includes(alumno)) {
+            cantidadTotal++;
         }
     }
-    return cantidadTotal
+    return `<b style='color:blue'> ${alumno}</b> esta en: <b>${cantidadTotal} clases</b></br>`;
 }
 
 mostrarInformacion("fisica")
@@ -42,4 +41,5 @@ mostrarInformacion("programacion")
 mostrarInformacion("logica")
 mostrarInformacion("quimica")
 
-document.write("cofla")
+document.write(cantidadClases("maria"))
+document.write(cantidadClases("pedro"))
